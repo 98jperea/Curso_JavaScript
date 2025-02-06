cac7ec80083cb867a098cdbc7458fd30c682e219
José Perea Bueno <98941059+98jperea@users.noreply.github.com>
2025-02-06 15:21:04 +0100

    Add files via upload

--- 22-classes.js
+++ 22-classes.js
@@ -29,7 +29,7 @@ console.log(typeof person)
 
 class DefaultPerson {
 
-    constructor(name = "Nombre por defecto", age, alias) {
+    constructor(name = "Nombre por defecto", age = 0, alias = "Sin alias") {
         this.name = name
         this.age = age
         this.alias = alias
@@ -37,5 +37,50 @@ class DefaultPerson {
 
 }
 
-let person3 = new DefaultPerson()
+let person3 = new DefaultPerson("Brais", 37)
 console.log(person3)
+
+// Acceso a propiedades
+
+console.log(person3.alias)
+console.log(person3["alias"])
+
+person3.alias = "MoureDev"
+console.log(person3.alias)
+
+// Funciones en clases
+
+class PersonWithMethod {
+
+    constructor(name, age, alias) {
+        this.name = name
+        this.age = age
+        this.alias = alias
+    }
+
+    walk() {
+        console.log("La persona camina.")
+    }
+
+}
+
+let person4 = new PersonWithMethod("Brais", 37, "MoureDev")
+person4.walk()
+
+// Propiedades privadas
+
+class PrivatePerson {
+
+    constructor(name, age, alias, bank) {
+        this.name = name
+        this.age = age
+        this.alias = alias
+        this.bank = bank
+    }
+
+    pay() {
+        this.bank
+    }
+}
+
+let person5 = new PrivatePerson("Brais", 37, "MoureDev", "IBAN123456789")
